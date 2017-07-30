45b554892ed14b913211d0f93f0585a3f8e9f1bc
Bilal Itani <bilalitani1@gmail.com>
2017-07-30 17:05:40 -0400

    Add finished project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Copyright (C) Xenfinity LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bilal Itani <bilalitani1@gmail.com>, June 2017
+ */
+
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask groundLayers;
+
+    public float speed = 5;
+
+    public float jumpForce = 7;
+
+    private Rigidbody rb;
+
+    private SphereCollider col;
+
+    #region Monobehaviour API
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
+
+    }
+
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }       
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
+    #endregion
+}
